d389ec3e92ee90b613725e2e0388ce9456912b95
msrusse <30694747+msrusse@users.noreply.github.com>
2018-04-06 12:42:12 -0500

    Updates the app to correct issues 26 and 27

--- app/src/main/java/com/example/appcenter/companion/examprep/ExamPrepOptionsActivity.java
+++ app/src/main/java/com/example/appcenter/companion/examprep/ExamPrepOptionsActivity.java
@@ -1,16 +1,23 @@
 package com.example.appcenter.companion.examprep;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.appcenter.companion.R;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 
@@ -18,6 +25,7 @@ public class ExamPrepOptionsActivity extends AppCompatActivity implements View.O
     NumberPicker numberPicker;
     String chapterNumbers[];
     RadioGroup studyDeckRadioGroup,feedbackRadioGroup;
+    RadioButton yesOption, immediateAnswersOption;
     public static final String KEY_SELECTED_OPTIONS_DATA = "com.example.appcenter.companion.OPTIONS_DATA";
     public static final String KEY_SELECTED_CHAPTERS_DATA = "com.example.appcenter.companion.CHAPTERS_DATA";
 
@@ -33,12 +41,16 @@ public class ExamPrepOptionsActivity extends AppCompatActivity implements View.O
 
         studyDeckRadioGroup = (RadioGroup)findViewById(R.id.study_deck_radio_group);
         feedbackRadioGroup = (RadioGroup)findViewById(R.id.feedback_radio_group);
+        yesOption = (RadioButton)findViewById(R.id.option_1);
+        immediateAnswersOption = (RadioButton) findViewById(R.id.feedback_option_1);
         Button saveButton =(Button)findViewById(R.id.save_button);
-
+        yesOption.setChecked(true);
+        immediateAnswersOption.setChecked(true);
         numberPicker=(NumberPicker)findViewById(R.id.number_picker);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(Integer.parseInt(chapterNumbers[chapterNumbers.length-1]));
         numberPicker.setValue(Integer.parseInt(chapterNumbers[chapterNumbers.length-1]));
+        setNumberPickerTextColor(numberPicker);
         saveButton.setOnClickListener(this);
     }
 
@@ -60,5 +72,34 @@ public class ExamPrepOptionsActivity extends AppCompatActivity implements View.O
                                     }
                 break;
         }
+    }
+
+    public static boolean setNumberPickerTextColor(NumberPicker numberPicker)
+    {
+        final int count = numberPicker.getChildCount();
+        for(int i = 0; i < count; i++){
+            View child = numberPicker.getChildAt(i);
+            if(child instanceof EditText){
+                try{
+                    Field selectorWheelPaintField = numberPicker.getClass()
+                            .getDeclaredField("mSelectorWheelPaint");
+                    selectorWheelPaintField.setAccessible(true);
+                    ((Paint)selectorWheelPaintField.get(numberPicker)).setColor(Color.WHITE);
+                    ((EditText)child).setTextColor(Color.WHITE);
+                    numberPicker.invalidate();
+                    return true;
+                }
+                catch(NoSuchFieldException e){
+                    Log.w("setNumPickerTextColor", e);
+                }
+                catch(IllegalAccessException e){
+                    Log.w("setNumPickerTextColor", e);
+                }
+                catch(IllegalArgumentException e){
+                    Log.w("setNumPickerTextColor", e);
+                }
+            }
+        }
+        return false;
     }
 }
