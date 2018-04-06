2bebdbdc42da33d0e732febd0d1209d76a605e8c
msrusse <30694747+msrusse@users.noreply.github.com>
2018-04-06 13:53:16 -0500

    Updates the app to correct issues 37, 38, 42

--- app/src/main/java/com/example/appcenter/companion/MainActivity.java
+++ app/src/main/java/com/example/appcenter/companion/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         DataBaseHelper myDbHelper ;
         SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
 
